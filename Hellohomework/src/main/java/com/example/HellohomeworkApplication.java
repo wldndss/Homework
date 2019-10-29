3f8150fc3489142bb88f40a5e330ff65979d7a97
3hagnyeon <3hagnyeon@117.17.40.186>
2019-10-29 16:31:07 +0900

    Homework

---
+++ Hellohomework/src/main/java/com/example/HellohomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellohomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellohomeworkApplication.class, args);
+	}
+
+}
